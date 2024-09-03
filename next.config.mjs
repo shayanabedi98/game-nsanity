bc25bededcec50824e4251ff21dceebdb3ec6816
shayanabedi98 <shayanabedi@hotmail.com>
2024-09-03 17:38:17 -0400

    changes to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["lh3.googleusercontent.com", "res.cloudinary.com"]
-    }
+  images: {
+    domains: ["lh3.googleusercontent.com", "res.cloudinary.com"],
+  },
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
 };
 
 export default nextConfig;
